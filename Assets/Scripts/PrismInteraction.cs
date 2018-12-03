f9526906e9e3d6cdaddd86dabf61500189c21319
maticlikar <matic@likar.me>
2018-12-03 10:11:00 -0800

    Anything tagged 'prism' will rotate whenever the player's trigger box enters it.

--- Assets/Scripts/PrismInteraction.cs
+++ Assets/Scripts/PrismInteraction.cs
@@ -6,11 +6,19 @@ public class PrismInteraction : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.tag == "Prism")
+        {
+            collision.transform.Rotate(new Vector3(0, 0, 45));
+        }
+    }
 }
