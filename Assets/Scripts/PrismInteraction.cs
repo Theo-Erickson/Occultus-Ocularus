e64d96496c62b99bc1701401718c2eb108f5a480
maticlikar <matic@likar.me>
2019-01-11 18:33:50 -0800

    Made the program simpler.

--- Assets/Scripts/PrismInteraction.cs
+++ Assets/Scripts/PrismInteraction.cs
@@ -12,22 +12,19 @@ public class PrismInteraction : MonoBehaviour {
 
     [Tooltip("Object with this tag will be rotated")] 
     public string rotateTag = "Prism";
-
+        
     [Tooltip("How much to rotate the object by")]
     public int degrees = 45;
 
     [Tooltip("Which key is used for rotation")] [SerializeField] 
     public KeyCode keyCode = KeyCode.E;
 
-    // Is the player in range to rotate object (e.i. is the trigger inside)
-    private bool isInRange = false;
-
     // All of the objects that the player is currently "colliding" with (e.i. the objects to possibly rotate)
     private List<GameObject> currentlyColliding = new List<GameObject>();
 
     void Update() {
         // If key pressed, in range, and colliding object is not null...
-        if (Input.GetKeyDown(keyCode) && isInRange)
+        if (Input.GetKeyDown(keyCode))
         {
             // If the thing with which you are colliding with is tagged correctly, rotate it
             for (int i = 0; i < currentlyColliding.Count; i++)
@@ -47,14 +44,10 @@ public class PrismInteraction : MonoBehaviour {
 
     // Rotates anything tagged 'tagToRotate' when E is pressed
     private void OnTriggerEnter2D(Collider2D collision) {
-        isInRange = true;
-        
         currentlyColliding.Add(collision.transform.gameObject);
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
-        isInRange = false;
-
         currentlyColliding.Remove(collision.transform.gameObject);
     }
 }
