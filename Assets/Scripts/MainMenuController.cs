fd9fdaf2f8ed361de9ecb40c0dd2c8304a224ade
maticlikar <matic@likar.me>
2019-02-15 18:54:01 -0800

    Menu screen and character selection working.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Tooltip("Name of scene to load when 'Play' is clicked")]
+    [SerializeField]
+    public string firstScene = "LayerSwitchingExample";
+
+    [Tooltip("Player prefab")]
+    [SerializeField]
+    public GameObject player;
+
+    [Tooltip("Sprites for the two characters")]
+    [SerializeField]
+    public Sprite[] sprites = new Sprite[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCharacter(PlayerPrefs.GetInt("character"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(firstScene);
+    }
+
+    public void ChangeCharacter(int character)
+    {
+        PlayerPrefs.SetInt("character", character);
+
+        SetCharacter(character);
+    }
+
+    public void SetCharacter(int character)
+    {
+        if (character == 0)
+        {
+            player.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+        else
+        {
+            player.GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
+    }
+}
+
