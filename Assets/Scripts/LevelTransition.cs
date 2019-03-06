66196ac2995e976ed8a882fbbc79f2aaab8875cf
Ray Nelson <rayolinels@gmail.com>
2019-03-05 17:01:09 -0800

    LevelTransition prefab sends the player to another scene
    …when the player collides with it
    - Level transition added between MallIntro and Rooftops scenes, and at the end of rooftops you return to the main menu
    - Also UI simplified and now has gradient to make it more visible
    -Added MallIntro, Rooftops, and MainMenu to build settings

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Move to next scene");
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextScene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
