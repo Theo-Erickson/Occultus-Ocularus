32ed1e168178aca4c70ca837a0a42a6db52d0906
eric <eric2000million@yahoo.com>
2019-04-19 19:43:20 -0700

    restructure nested if statements for spacebar input

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -42,25 +42,30 @@ public class Dialogue : MonoBehaviour
 
     void Update()
     {
-        if (text != null) {
+        if (text != null)
+        {
 
             // Do dialogue action for this phrase if there is one
             if (phraseIndex < phrases.Length &&
                 actions[phraseIndex] != null &&
                 !actionPerformed &&
-                !awaitingUser) {
+                !awaitingUser)
+            {
                 dialogueEncounter.DialogueAction(actions[phraseIndex]);
                 actionPerformed = true;
             }
 
             if (Input.GetKeyDown(KeyCode.Space){
+                // Resume normal speed text scrolling
+                if (awaitingUser)
+                    awaitingUser = false;
                 // Skip to end of line if button is pressed immediately after increasing the scroll speed
-                if (!awaitingUser && Time.time - lastScrollSpeedUpTime < SKIP_TO_END_TIME_RANGE)
+                else if (Time.time - lastScrollSpeedUpTime < SKIP_TO_END_TIME_RANGE)
                 {
                     skipToEndOfPhrase = true;
                 }
                 // Increase speed of text scrolling
-                else if (!awaitingUser)
+                else
                 {
                     currentScrollRate = FAST_SCROLL_RATE;
                     lastScrollSpeedUpTime = Time.time;
@@ -76,12 +81,7 @@ public class Dialogue : MonoBehaviour
                         player.canMove = true;
                     }
                 }
-                // Resume normal speed text scrolling
-                else
-                {
-                    awaitingUser = false;
-                }
-            }                
+            }
 
             // Make scroll rate normal again if space key is released
             if (Input.GetKeyUp(KeyCode.Space))
@@ -90,12 +90,14 @@ public class Dialogue : MonoBehaviour
             // Make a new letter appear after each interval defined by SCROLL_RATE
             while (Time.time - lastUpdateTime > currentScrollRate &&
                    phraseIndex < phrases.Length &&
-                   !awaitingUser) {
+                   !awaitingUser)
+            {
                 string phrase = phrases[phraseIndex];
                 lastUpdateTime = Time.time;
 
                 // Fill in the rest of the current phrase
-                if (skipToEndOfPhrase) {
+                if (skipToEndOfPhrase)
+                {
                     text.text = phrase;
                     charIndex = -1;
                     phraseIndex++;
@@ -103,13 +105,15 @@ public class Dialogue : MonoBehaviour
                     skipToEndOfPhrase = false;
                     actionPerformed = false;
                 }
-                else {
+                else
+                {
                     text.text = phrase.Substring(0, 1 + charIndex);
                     charIndex++;
                 }
 
                 // Prep for next phrase when the end of current phrase is reached
-                if (charIndex == phrase.Length) {
+                if (charIndex == phrase.Length)
+                {
                     charIndex = -1;
                     phraseIndex++;
                     awaitingUser = true;
@@ -125,7 +129,8 @@ public class Dialogue : MonoBehaviour
 
         int numPhrases = phrasesAndActions.Length;
 
-        for (int i = 0; i < phrasesAndActions.Length; i++) {
+        for (int i = 0; i < phrasesAndActions.Length; i++)
+        {
             string phrase = phrasesAndActions[i].Trim();
             if (phrase.StartsWith("{") && phrase.EndsWith("}"))
                 numPhrases--;
@@ -134,9 +139,11 @@ public class Dialogue : MonoBehaviour
         phrases = new string[numPhrases];
 
         int diff = 0;
-        for (int i = 0; i < phrasesAndActions.Length; i++) {
+        for (int i = 0; i < phrasesAndActions.Length; i++)
+        {
             string phrase = phrasesAndActions[i].Trim();
-            if (phrase.StartsWith("{") && phrase.EndsWith("}")) {
+            if (phrase.StartsWith("{") && phrase.EndsWith("}"))
+            {
                 actions.SetValue(phrase.Trim(new char[2] { '{', '}' }), i - diff);
                 diff++;
             }
