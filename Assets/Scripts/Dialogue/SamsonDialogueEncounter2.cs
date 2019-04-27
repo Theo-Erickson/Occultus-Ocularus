1b45da9e865f96a27af691a90be37f827e8f73f3
Ray Nelson <rayolinels@gmail.com>
2019-04-27 00:02:25 -0700

    controls UI now has layer switching controls
    - Re-enabled UI and Samson on Outside Mall+TTD, MPT scene
    - Controls overlay now properly reveals camera controls when first applicable, as it did before

--- Assets/Scripts/Dialogue/SamsonDialogueEncounter2.cs
+++ Assets/Scripts/Dialogue/SamsonDialogueEncounter2.cs
@@ -6,25 +6,16 @@ public class SamsonDialogueEncounter2 : MonoBehaviour, IDialogueEncounter
 {
     public TextAsset dialogueText;
     public Dialogue dialogueSetup;
-
-    public PlayerController player;
     public LevelTransition levelTransition;
     public Light cityLight;
 
     private bool talkedToSamson;
     private bool lightsOut;
 
-    private void Start()
-    {
-        player.canMove = false;
-    }
-
     private void Update()
     {
         if (lightsOut && cityLight.intensity >= 8)
             cityLight.intensity -= 1f;
-        if (!talkedToSamson)
-            player.canMove = false;
     }
 
     public void Talk()
@@ -40,6 +31,7 @@ public class SamsonDialogueEncounter2 : MonoBehaviour, IDialogueEncounter
         levelTransition.FadeAway(GetComponent<SpriteRenderer>());
         foreach(BoxCollider2D bc in GetComponents<BoxCollider2D>())
             bc.enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
     }
 
     public void DialogueAction(string action)
