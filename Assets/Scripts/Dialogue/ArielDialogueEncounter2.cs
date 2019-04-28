a61e5dbe1209245468fbcf34eb4ad7d637d06800
Ray Nelson <rayolinels@gmail.com>
2019-04-27 21:48:21 -0700

    Ariel dialogue has Samson appear and disappear

---
+++ Assets/Scripts/Dialogue/ArielDialogueEncounter2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArielDialogueEncounter2 : MonoBehaviour, IDialogueEncounter
+{
+    public TextAsset dialogueText;
+    public Dialogue dialogueSetup;
+
+    public SpriteRenderer samson;
+    public LevelTransition fadeEffect;
+
+    public void Talk()
+    {
+        Dialogue dialogueInstance = dialogueSetup.ActivateDialogueBox();
+        dialogueInstance.Setup(this);
+        dialogueInstance.ParseMessage(dialogueText.ToString());
+    }
+
+    public void DialogueAction(string action)
+    {
+        if (action.Equals("Samson appears"))
+            fadeEffect.FadeAppear(samson);
+        else if (action.Equals("Samson disappears"))
+            fadeEffect.FadeAway(samson);
+        else
+            Debug.Log("DialogAction: " + action);
+    }
+
+    public void DialogueFinished() {}
+}
