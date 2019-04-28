bf122408d349e082e4a4a7849e799ec2e978bba1
Seiji Emery <seijiemery@gmail.com>
2019-04-28 02:01:19 -0700

    reverted previous commit to re-enable gamepad hotswapping

--- Assets/Scripts/UIDisplay.cs
+++ Assets/Scripts/UIDisplay.cs
@@ -9,62 +9,84 @@ public class UIDisplay : MonoBehaviour {
 
     private GameObject minCtrls;
     private GameObject detailedCtrls;
-    private bool configured;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Start() wasn't always picking up on the gamepad for some reason
-        if (!configured) {
-            // Switch between gamepad and keyboard controls overlays depending on if a controller is connected
-            if (Gamepad.current != null) {
-                print(Gamepad.current);
-                minCtrls = gameObject.transform.GetChild(1).GetChild(0).gameObject;
-                detailedCtrls = gameObject.transform.GetChild(1).GetChild(1).gameObject;
-                gameObject.transform.GetChild(0).gameObject.SetActive(false);
-                gameObject.transform.GetChild(1).gameObject.SetActive(true);
-            }
-            else {
-                minCtrls = gameObject.transform.GetChild(0).GetChild(0).gameObject;
-                detailedCtrls = gameObject.transform.GetChild(0).GetChild(1).gameObject;
-                gameObject.transform.GetChild(0).gameObject.SetActive(true);
-                gameObject.transform.GetChild(1).gameObject.SetActive(false);
-            }
+    // Track whether a gamepad was connected last update, see UpdateControlsUI
+    private bool gamepadConnected = false;
 
-            // Turn on layer switching controls if applicable to current level
-            if (SceneManager.GetActiveScene().path.Contains("2nd Area - Suburbs")) {
-                minCtrls.transform.GetChild(1).gameObject.SetActive(false);
-                minCtrls.transform.GetChild(2).gameObject.SetActive(true);
-            }
-            else {
-                minCtrls.transform.GetChild(1).gameObject.SetActive(true);
-                minCtrls.transform.GetChild(2).gameObject.SetActive(false);
-            }
+    // Call to update UI to reflect whether a gamepad is currently plugged in or not
+    // If yes, displays UI for gamepad controls, and if not, displays UI for keyboard controls
+    // This gets called at Start() and by Update(), but only when the current state of the attached gamepad changes.
+    private void UpdateControlsUI() {
+        // use this to monitor when the gamepad status changes, see Update()
+        gamepadConnected = Gamepad.current != null;
 
-            // Turn on camera zoom controls if applicable to current level
-            if (SceneManager.GetActiveScene().name.Equals("TwoLaserDoorLayer") ||
-                SceneManager.GetActiveScene().name.Equals("Outside Mall+TTD, MPT")) {
-                minCtrls.transform.GetChild(3).gameObject.SetActive(true);
-            }
-            else {
-                minCtrls.transform.GetChild(3).gameObject.SetActive(false);
-            }
-            configured = true;
+        // Switch between gamepad and keyboard controls overlays depending on if a controller is connected
+        if (Gamepad.current != null)
+        {
+            print(Gamepad.current);
+            minCtrls = gameObject.transform.GetChild(1).GetChild(0).gameObject;
+            detailedCtrls = gameObject.transform.GetChild(1).GetChild(1).gameObject;
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            minCtrls = gameObject.transform.GetChild(0).GetChild(0).gameObject;
+            detailedCtrls = gameObject.transform.GetChild(0).GetChild(1).gameObject;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
         }
 
+        // Turn on layer switching controls if applicable to current level
+        if (SceneManager.GetActiveScene().path.Contains("2nd Area - Suburbs"))
+        {
+            minCtrls.transform.GetChild(1).gameObject.SetActive(false);
+            minCtrls.transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else
+        {
+            minCtrls.transform.GetChild(1).gameObject.SetActive(true);
+            minCtrls.transform.GetChild(2).gameObject.SetActive(false);
+        }
 
+        // Turn on camera zoom controls if applicable to current level
+        if (SceneManager.GetActiveScene().name.Equals("TwoLaserDoorLayer") ||
+            SceneManager.GetActiveScene().name.Equals("Outside Mall+TTD, MPT"))
+        {
+            minCtrls.transform.GetChild(3).gameObject.SetActive(true);
+        }
+        else
+        {
+            minCtrls.transform.GetChild(3).gameObject.SetActive(false);
+        }
+    }
+
+    public void Start() {
+        UpdateControlsUI();
+    }
+    public void Update() {
+        // If the last observed state of whether a gamepad was detected does not match the current state, update the
+        // controls UI. Yes, we could do this with callbacks, but this is simpler and not likely to cause errors.
+        if (gamepadConnected != (Gamepad.current != null)) {
+            UpdateControlsUI();
+        }
+        
         // Switch between minimalist and detailed controls UI when Tab is pressed
-        if (Input.GetKeyDown(KeyCode.Tab)) {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
 
-            if (!minCtrls.activeSelf && !detailedCtrls.activeSelf || minCtrls.activeSelf && detailedCtrls.activeSelf) {
+            if (!minCtrls.activeSelf && !detailedCtrls.activeSelf || minCtrls.activeSelf && detailedCtrls.activeSelf)
+            {
                 minCtrls.SetActive(true);
                 detailedCtrls.gameObject.SetActive(false);
             }
-            else if (minCtrls.activeSelf && !detailedCtrls.activeSelf) {
+            else if (minCtrls.activeSelf && !detailedCtrls.activeSelf)
+            {
                 minCtrls.SetActive(false);
                 detailedCtrls.gameObject.SetActive(true);
             }
-            else if (!minCtrls.activeSelf && detailedCtrls.activeSelf) {
+            else if (!minCtrls.activeSelf && detailedCtrls.activeSelf)
+            {
                 minCtrls.SetActive(false);
                 detailedCtrls.gameObject.SetActive(false);
             }
