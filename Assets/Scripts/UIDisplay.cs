810249d3958697a2c766e2dbebe6ab6e7a0f06a7
Ray Nelson <rayolinels@gmail.com>
2019-04-28 00:38:24 -0700

    Controls overlay now autoswitches between keyboard/gamepad
    - Switches display of controls based on whether a gameoad is attached or not
    - Also checks the scene name / scene folder name to see if it should show camera or layer switching controls

--- Assets/Scripts/UIDisplay.cs
+++ Assets/Scripts/UIDisplay.cs
@@ -1,22 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Input;
+using UnityEngine.SceneManagement;
 
-public class UIDisplay : MonoBehaviour
-{
-    GameObject minCtrls;
-    GameObject detailedCtrls;
+public class UIDisplay : MonoBehaviour {
+    public PlayerInputMapping playerInput;
+
+    private GameObject minCtrls;
+    private GameObject detailedCtrls;
 
     // Start is called before the first frame update
     void Start()
     {
-        minCtrls = gameObject.transform.GetChild(0).gameObject;
-        detailedCtrls = gameObject.transform.GetChild(1).gameObject;
+        // Switch between gamepad and keyboard controls overlays depending on if a controller is connected
+        if (Gamepad.current != null) {
+            minCtrls = gameObject.transform.GetChild(1).GetChild(0).gameObject;
+            detailedCtrls = gameObject.transform.GetChild(1).GetChild(1).gameObject;
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else {
+            minCtrls = gameObject.transform.GetChild(0).GetChild(0).gameObject;
+            detailedCtrls = gameObject.transform.GetChild(0).GetChild(1).gameObject;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        }
+
+        // Turn on layer switching controls if applicable to current level
+        if (SceneManager.GetActiveScene().path.Contains("2nd Area - Suburbs")) {
+            minCtrls.transform.GetChild(1).gameObject.SetActive(false);
+            minCtrls.transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else {
+            minCtrls.transform.GetChild(1).gameObject.SetActive(true);
+            minCtrls.transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        // Turn on camera zoom controls if applicable to current level
+        if (SceneManager.GetActiveScene().name.Equals("TwoLaserDoorLayer") ||
+            SceneManager.GetActiveScene().name.Equals("Outside Mall+TTD, MPT")) {
+            minCtrls.transform.GetChild(3).gameObject.SetActive(true);
+        }
+        else {
+            minCtrls.transform.GetChild(3).gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // Switch between minimalist and detailed controls UI when Tab is pressed
         if (Input.GetKeyDown(KeyCode.Tab)) {
 
