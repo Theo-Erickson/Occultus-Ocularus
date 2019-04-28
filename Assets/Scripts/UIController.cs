355f9fcde24255b6c67de4fe747f308f5062cb01
Ray Nelson <rayolinels@gmail.com>
2019-04-28 11:19:27 -0700

    Changed menus to use keyboard/button controls

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.Input;
+
+public class UIController : MonoBehaviour, IUIActions {  
+      
+    public PlayerInputMapping uiInput;
+    public LevelTransition levelTransition;
+    public PlayStats playStats;
+    public MainMenuController mainMenuController;
+
+    private string currentScene;
+
+    public void OnCancel(InputAction.CallbackContext context) {
+        if (currentScene.Equals("End Menu")) {
+            mainMenuController.QuitGame();
+        }
+    }
+
+    public void OnClick(InputAction.CallbackContext context) {
+
+    }
+
+    public void OnNavigate(InputAction.CallbackContext context) {
+
+    }
+
+    public void OnOpenMenu(InputAction.CallbackContext context) {
+        if (currentScene.Equals("End Menu")) {
+            mainMenuController.QuitGame();
+        }
+    }
+
+    public void OnPoint(InputAction.CallbackContext context) {
+
+    }
+
+    public void OnSubmit(InputAction.CallbackContext context) {
+        if (currentScene.Equals("End Menu")) {
+            levelTransition.FadeLoadScene();
+            playStats.RestartGame();
+        }
+        else {
+            levelTransition.FadeLoadScene();
+            playStats.CheckpointReached("Main Menu");
+        }
+
+    }
+
+    void Awake() {
+        uiInput.Enable();
+        uiInput.UI.SetCallbacks(this);
+    }
+
+    // Use this for initialization
+    void Start() {
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+}
